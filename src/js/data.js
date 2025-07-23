79ac47ba233d02d5e34ac3d59ebbee333dc4cc66
Egor Rudakov <egor.rudakov90@gmail.com>
2025-07-23 19:45:21 +0300

    refactor(data): расширил объект данных UI элементами управления плеером
    - Добавлены новые селекторы для элементов управления:
      • currentTimeSpan - текущее время трека
      • durationSpan - общая длительность трека
      • songInfoGrid - контейнер информации о треке
      • songName - название трека
      • songArtist - исполнитель
      • songYear - год выпуска
      • songImage - обложка трека
    - Удалены устаревшие селекторы:
      • rewindBackwardBtn
      • rewindForwardBtn
    - Улучшена функция createDataObject:
      • Более лаконичная реализация через Object.entries
      • Сохранение массива для infoButtonIconPlay
      • Автоматическое определение типа элемента (одиночный/коллекция)

--- src/js/data.js
+++ src/js/data.js
@@ -1,40 +1,3 @@
-// function createDataObject(selector) {
-//   const data = {};
-//   for (let key in selector) {
-//     if (key === 'infoButtonIconPlay') {
-//       data[key] = Array.from(document.querySelectorAll(selector[key]));
-//     } else {
-//       const elements = document.querySelectorAll(selector[key]);
-//       data[key] = elements.length === 1 ? elements[0] : elements;
-//     }
-//   }
-
-//   return data;
-// }
-
-// const selector = {
-//   seekBar: '.track-controls__seek-bar',
-//   outputPower: '.track-controls__output-power',
-//   volSeekBar: '.volume__seek-bar',
-//   volOutputPower: '.volume__output-power',
-//   volumeButton: '.volume__button',
-//   volumeSliderClass: '.volume__slider',
-//   controlsButton: '.controls__button--play',
-//   infoButtonIconPlay: '.info__button-icon-play',
-//   iconPlay: '.icon__play',
-//   iconPause: '.icon__pause',
-//   audioSliderWrapper: '.audio-slider__wrapper',
-//   previousTrackBtn: '#previous-track',
-//   playPauseBtn: '#play-pause',
-//   nextTrackBtn: '#next-track',
-//   volumeSliderId: '#volume-slider',
-//   rewindBackwardBtn: '#rewind-backward',
-//   rewindForwardBtn: '#rewind-forward',
-// };
-
-// const data = createDataObject(selector);
-
-// export default data;
 const createDataObject = (selector) => {
   return Object.fromEntries(
     Object.entries(selector).map(([key, value]) => {
@@ -67,8 +30,13 @@ const selector = {
   playPauseBtn: '#play-pause',
   nextTrackBtn: '#next-track',
   volumeSliderId: '#volume-slider',
-  rewindBackwardBtn: '#rewind-backward',
-  rewindForwardBtn: '#rewind-forward',
+  currentTimeSpan: '.track-controls__current-time',
+  durationSpan: '.track-controls__duration',
+  songInfoGrid: '.song-info__grid',
+  songName: '.song-info__song-name',
+  songArtist: '.song-info__song-artist',
+  songYear: '.song-info__song-year',
+  songImage: '.song-info__image',
 };
 
 const data = createDataObject(selector);
